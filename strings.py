2aee36a6988c080633bf09d676ba37ede5bc7001
kushalsharma-09 <sharmakushal0305@gmail.com>
2025-07-27 21:43:03 +0530

    string function

---
+++ strings.py
@@ -0,0 +1,77 @@
+# a='123654789dscsc@#s25'
+# print (a)
+
+
+# a="123654789dscsc@#s25"
+# print (a)
+
+# a="""
+# 123654789dscsc@#s25
+#  print (hoii)
+# """
+
+# str1= "hi"
+
+# str2= "by"
+# result=str1+str2
+# print(result)
+
+
+# str2= "by"
+# result=str2*5
+# print(result)
+
+# my_string ="himyi"
+# print(my_string[1:4:2])
+
+# print(my_string[-4])
+
+
+# my_string="lucky jangid"
+# print(len(my_string))
+
+
+# my_string="hello guys "
+# print ("hello"in my_string)
+# print ("hiii"in my_string)
+
+# string ="lucky jangid "
+# print(string [0])
+# print(string [4:9])
+# print(string [:6:2])
+
+# str1="lucky"
+# str2="jangid"
+# print(str1+" "+str2)
+
+# a="my"
+# a="hhii"
+# print(a)
+
+# a="my"
+# a="h"+ a[1:]
+# print(a)
+
+# my_string ="lucky"
+# print(my_string.upper())
+
+
+# my_string ="luCKy"
+# print(my_string.lower())
+
+
+# my_string="   lucky   jangid"
+# print(my_string.strip())
+
+
+# my_string="   lucky   jangid"
+# print(my_string.replace("uc","o"))
+
+# my_string= "hello lucky"
+# print(my_string.split())    #['hello', 'lucky']
+
+# my_string="my name is lucky"
+# print(my_string.split())     # ['my', 'name', 'is', 'lucky']
+
+words=["hello","lucky"]
+print("".join(words))
