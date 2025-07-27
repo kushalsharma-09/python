c2fd7f8745754a21dc9917b8a9de1fd80fbd5df0
kushalsharma-09 <sharmakushal0305@gmail.com>
2025-07-27 21:41:22 +0530

    string practise

---
+++ string_prec.py
@@ -0,0 +1,51 @@
+#  palindrome checker
+
+# data ="lucky"
+# print (data[::-1])
+
+# if data ==data[::-1]:
+#     print("it's a palindrome")
+#     else:
+#     print("it's not a palindrome")
+
+
+# sentence word counter:
+# Ask the user to enter sentences one by one .
+# use a while loop to keep accepting input until the user types.
+# "stop " (case-insensitive).
+# for each sentence entered ,use an if-else statement to check if the sentence contains more than 5 words.
+# print the sentence and indicte if it's "long  sentence!" or "short sentence!".
+# (hint : can split the sentence into words using split()and then check the length of the resulting list ).
+
+# while True:
+#     data = input("enter your sentence: ")
+#     if data. lower  =="stop":
+#         break
+
+#     if data.count(" ")>=5:
+#         print("it,s a long statement.")
+#     else:
+#         print("it,s a short sentence ")
+
+    
+# ask the user to type a string . use a while loop to process characters one by one 
+# (you can iterate using an index that increments ). maintain counts for :
+
+# uppercase letters 
+# lowercase letters 
+# digits 
+# special charaters (anything else )
+# use if-elif-else statements to categorize each character
+# stop the loop if you encounter the character "#"
+# finally ,print the counts for each type or charater
+
+count_upper =count_lower = count_ditit = count_spl = 0
+
+data="zxcvbn7962cv"
+length =len(data)
+
+counter=0
+
+while count_lower <length
+print(counter)
+counter +=1
